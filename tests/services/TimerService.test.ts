b27f14bd82d71a08ecc5be0b5f1236fe38ac8658
marsender <didier.corbiere@opale-concept.com>
2023-11-08 13:56:26 +0100

    format code via linter

--- tests/services/TimerService.test.ts
+++ tests/services/TimerService.test.ts
@@ -1,7 +1,7 @@
-import TimerService from "../../src/services/TimerService"
+import TimerService from '../../src/services/TimerService'
 
 test('timer get set', () => {
 	const timerService = new TimerService()
 	timerService.setTimer(12)
-	expect(timerService.getTimer()).toBe(12);
-});
+	expect(timerService.getTimer()).toBe(12)
+})
