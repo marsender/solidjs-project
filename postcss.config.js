301b2d8a435edddb2f6d5239b771530372587dd5
marsender <didier.corbiere@opale-concept.com>
2025-09-16 17:17:29 +0200

    Upgrade tailwind css #10

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
+import tailwindcss from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer'
+
 export default {
-	plugins: {
-		'postcss-import': {},
-		tailwindcss: {},
-		autoprefixer: {},
-	},
+	plugins: [tailwindcss, autoprefixer],
 }
